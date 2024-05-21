896abf2860d2c8a617e61db63cb96091c306bc8d
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-05-21 20:07:07 -0300

    Audits page style inline.

--- my-portfolio/my-portfolio-client/src/components/Audits.js
+++ my-portfolio/my-portfolio-client/src/components/Audits.js
@@ -1,34 +1,55 @@
 import React from 'react';
-import './Audits.css';
 
 const Audits = () => {
   const apps = [
     {
-      name: 'CCex-Staking-Platform',
+      name: 'CCex Bank Staking Platform',
       image: 'https://i.ibb.co/PcWKQTq/Decentralized-stacking-platform-Audit-3-hours.jpg',
-      description: 'Decentralized stacking platform. - Audit 3 hours. - Click Image to see the project',
+      description: 'Decentralized Bank Staking Platform. - Audit 3 hours. - Click Image to see the project',
       link: 'https://github.com/gonzalolater/CCex-Staking-Platform'
     },
     {
-      name: 'DeFiExchange with Node v16.20.0',
+      name: 'DeFi Exchange with Node v16.20.0',
       image: 'https://i.ibb.co/sjrGPj2/img-4.png',
-      description: 'Decentralized exchange - 8 hours audit. - Click on the image to see the project and read all the Issues and Recommendations.',
+      description: 'Decentralized Financial Exchange - 8 hours audit. - Click on the image to see the project and read all the Issues and Recommendations.',
       link: 'https://github.com/gonzalolater/DeFiExchange'
     },
     // Agrega más objetos para cada aplicación que desees mostrar
   ];
 
   return (
-    <div className="auditsContainer" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h1>Auditorys</h1>
-      <h1 style={{ textAlign: 'center'}}>Audited Projects</h1>
+    <div
+      style={{
+        marginBottom: '70px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1 style={{ textAlign: 'center', marginTop: '90px' }}>Audited Projects</h1>
       {apps.map((app, index) => (
-        <div style={{ textAlign: 'center' }} key={index}>
-          <h2 >{app.name}</h2>
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: '-30px',
+            marginBottom: '20px',
+            width: '80%',
+            maxWidth: '800px',
+            margin: '0 auto',
+          }}
+          key={index}
+        >
+          <h2 style={{
+            textDecoration: app.name === 'CCex Bank Staking Platform' || app.name === 'DeFi Exchange with Node v16.20.0' ? 'underline' : 'none'
+          }}>{app.name}</h2>
           <a href={app.link}>
-            <img src={app.image} alt={app.name} />
+            <img
+              src={app.image}
+              alt={app.name}
+              style={{maxWidth: '100%', height: 'auto', width: '100%'}}
+            />
           </a>
-          <p>{app.description}</p>
+          <p style={{fontWeight: 'bold', color: 'red'}}>{app.description}</p>
         </div>
       ))}
     </div>
