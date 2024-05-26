4082e007b767535a835a36049a6f9ccd5cf41919
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-05-26 14:04:54 -0300

    Give Format to SC

--- my-portfolio/my-portfolio-client/src/components/Contracts-item/contracts/portfolio-erc721-GDA.sol
+++ my-portfolio/my-portfolio-client/src/components/Contracts-item/contracts/portfolio-erc721-GDA.sol
@@ -7,7 +7,12 @@ import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721Enumerable.s
 import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
-contract portfolioErc721GDA is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+contract portfolioErc721GDA is
+    ERC721,
+    ERC721Enumerable,
+    ERC721Pausable,
+    Ownable
+{
     uint256 private _nextTokenId;
 
     uint public immutable maxSupply = 4;
@@ -16,10 +21,7 @@ contract portfolioErc721GDA is ERC721, ERC721Enumerable, ERC721Pausable, Ownable
 
     mapping(address => bool) public whiteList;
 
-    constructor()
-        ERC721("portfolio-erc721-GDA", "GDA")
-        Ownable(msg.sender)
-    {}
+    constructor() ERC721("portfolio-erc721-GDA", "GDA") Ownable(msg.sender) {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://QmThieq3BHYyTKWHzxJche4HW7aLytwT9G1BDrZYH2SBeK/";
@@ -43,40 +45,46 @@ contract portfolioErc721GDA is ERC721, ERC721Enumerable, ERC721Pausable, Ownable
 
     // Public Mint
     function publicMint() external payable {
-        require(publicMintOpen, "Window closed");        
+        require(publicMintOpen, "Window closed");
         require(msg.value == 0.01 ether, "Not enough ether send");
         _internalMint();
     }
 
     // Internal Function
     function _internalMint() internal {
-        require(totalSupply() < maxSupply,"No more NFTs");
+        require(totalSupply() < maxSupply, "No more NFTs");
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
     }
 
     // Update windows for minting
-    function updateMintWindows(bool _publicMintOpen, bool _whiteListMintOpen) external onlyOwner {
+    function updateMintWindows(
+        bool _publicMintOpen,
+        bool _whiteListMintOpen
+    ) external onlyOwner {
         publicMintOpen = _publicMintOpen;
         whiteListMintOpen = _whiteListMintOpen;
     }
 
     // Add Address to WhiteList
     function updateWhiteList(address[] memory _addresses) external onlyOwner {
-        for (uint256 i; i < _addresses.length; i++) 
-        {
+        for (uint256 i; i < _addresses.length; i++) {
             whiteList[_addresses[i]] = true;
         }
-    }  
+    }
 
     function withdraw(address _receiver) external onlyOwner {
         uint256 balance = address(this).balance;
         payable(_receiver).transfer(balance);
-    } 
+    }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address to, uint256 tokenId, address auth)
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    )
         internal
         override(ERC721, ERC721Enumerable, ERC721Pausable)
         returns (address)
@@ -84,19 +92,16 @@ contract portfolioErc721GDA is ERC721, ERC721Enumerable, ERC721Pausable, Ownable
         return super._update(to, tokenId, auth);
     }
 
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
         super._increaseBalance(account, value);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
