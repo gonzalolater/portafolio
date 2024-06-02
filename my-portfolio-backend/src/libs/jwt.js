9d033e01a95cbc491d2b6d5da3b903c7544ea883
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-06-01 21:23:25 -0300

    set 1 hour token

--- my-portfolio-backend/src/libs/jwt.js
+++ my-portfolio-backend/src/libs/jwt.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export async function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1h" }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
