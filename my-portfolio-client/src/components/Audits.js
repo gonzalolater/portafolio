3ceea02404b87a2b03d39dff9ea8f991fdea16a7
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-06-05 11:09:57 -0300

    North Korean hacking activity - Project Audited.

--- my-portfolio-client/src/components/Audits.js
+++ my-portfolio-client/src/components/Audits.js
@@ -26,7 +26,7 @@ const Audits = () => {
         alignItems: 'center',
       }}
     >
-      <h1 style={{ textAlign: 'center', marginTop: '110px' }}>Audited Projects</h1>
+      <h1 style={{ textAlign: 'center', marginTop: '110px' }}>Audited Projects - North Korean hacking activity</h1>
       {apps.map((app, index) => (
         <div
           style={{
