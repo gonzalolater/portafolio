cc9c7f8aef710b72abac24c6089697d0f5de6cc0
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-12-31 07:21:13 -0300

    change of style and addition of colors with translation at 1000 and maintaining responsiveness.

--- my-portfolio-client/src/components/Hamburguesa.js
+++ my-portfolio-client/src/components/Hamburguesa.js
@@ -1,37 +1,26 @@
 import React from 'react';
 
-const Hamburguesa = () => {
+const Hamburguesa = ({ color }) => {
+  const lineStyle = {
+    width: '30px',
+    height: '3px',
+    backgroundColor: color || '#333',
+    marginBottom: '5px',
+    transition: 'background-color 1s ease', // Mantiene la transición
+  };
+
   return (
-    <>
-    <div
-className="line"
-style={{
-    width: '30px',
-    height: '3px',
-    backgroundColor: '#333',
-    marginBottom: '5px',
-}}
-></div>
-<div
-className="line"
-style={{
-    width: '30px',
-    height: '3px',
-    backgroundColor: '#333',
-    marginBottom: '5px',
-}}
-></div>
-<div
-className="line"
-style={{
-    width: '30px',
-    height: '3px',
-    backgroundColor: '#333',
-}}
-></div>
-</>
+    <div style={{ // Añadimos un div contenedor con flex-direction: column
+      display: 'flex',
+      flexDirection: 'column', // Apila los elementos verticalmente
+      justifyContent: 'space-between', // Distribuye el espacio entre las líneas
+      height: '18px', // Altura fija para el contenedor
+    }}>
+      <div className="line" style={lineStyle}></div>
+      <div className="line" style={lineStyle}></div>
+      <div className="line" style={{ ...lineStyle, marginBottom: 0 }}></div>
+    </div>
   );
 };
 
 export default Hamburguesa;
-
