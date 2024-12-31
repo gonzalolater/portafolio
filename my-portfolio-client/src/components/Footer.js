565df873a15319eb67ff1bed03bf0967b83f2453
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2024-12-31 07:23:06 -0300

    Modified the footer by dividing CSS styles into a separate file, and added a responsive purple gradient.

--- my-portfolio-client/src/components/Footer.js
+++ my-portfolio-client/src/components/Footer.js
@@ -1,44 +1,15 @@
 import React from 'react';
 import ChatButton from './ChatButton';
+import './Footer.css'; // Importa el archivo CSS
 
 const Footer = () => {
-  const footerStyle = {
-    background: 'violet',
-    color: 'red',
-    textAlign: 'center',
-    padding: '1px',
-    position: 'fixed',
-    bottom: 0,
-    left: 0,
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingLeft: '20px',
-    paddingRight: '20px',
-  };
-
-  const h2Style = {
-    marginRight: '10px',
-    textAlign: 'center',
-    flex: 1,
-  };
-
-  const chatButtonStyle = {
-    marginRight: '40px',
-  };
-
   return (
-    <div>
-      <>
-        <footer style={footerStyle}>
-          <h2 style={h2Style}>My Portfolio Full Responsive</h2>
-          <div style={chatButtonStyle}>
-            <ChatButton />
-          </div>
-        </footer>
-      </>
-    </div>
+    <footer className="footer">
+      <h2>My Portfolio Full Responsive</h2>
+      <div className="chat-button-container">
+        <ChatButton />
+      </div>
+    </footer>
   );
 };
 
