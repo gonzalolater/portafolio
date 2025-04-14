814767f0d64d59c0d5d972f0c0bb1bc796918238
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2025-04-14 16:21:57 -0300

    Refactor Navbar component styles for consistency and readability

--- my-portfolio-client/src/components/Navbar.js
+++ my-portfolio-client/src/components/Navbar.js
@@ -25,23 +25,23 @@ const Navbar = () => {
       <nav>
         <div
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            padding: '10px',
-            backgroundColor: 'violet',
-            position: 'fixed',
-            top: '0',
-            left: '0',
-            width: '100%',
-            zIndex: '100',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "10px",
+            backgroundColor: "violet",
+            position: "fixed",
+            top: "0",
+            left: "0",
+            width: "100%",
+            zIndex: "100",
           }}
         >
           <div
             className="hamburger"
             onClick={toggleMenu} // Usamos toggleMenu aquí
             style={{
-              cursor: 'pointer',
+              cursor: "pointer",
             }}
           >
             <Hamburguesa color={colors[colorIndex]} />
@@ -49,39 +49,57 @@ const Navbar = () => {
           <ul
             className="menu"
             style={{
-              display: isOpen ? 'flex' : 'none',
-              flexDirection: 'column',
-              listStyle: 'none',
-              padding: '10px',
-              margin: '0 10px',
-              position: 'absolute',
-              top: '100%',
-              left: '0',
-              backgroundColor: 'violet',
-              boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-              zIndex: '200',
-              justifyContent: 'flex-end',
-              textDecoration: 'none',
-              color: 'inherit',
+              display: isOpen ? "flex" : "none",
+              flexDirection: "column",
+              listStyle: "none",
+              padding: "10px",
+              margin: "0 10px",
+              position: "absolute",
+              top: "100%",
+              left: "0",
+              backgroundColor: "violet",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+              zIndex: "200",
+              justifyContent: "flex-end",
+              textDecoration: "none",
+              color: "inherit",
             }}
           >
-            <li style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Link to="/audits" onClick={toggleMenu}>Audits</Link> {/* Añadido onClick */}
+            <li style={{ textDecoration: "none", color: "inherit" }}>
+              <Link to="/audits" onClick={toggleMenu}>
+                Audits
+              </Link>{" "}
+              {/* Añadido onClick */}
             </li>
-            <li style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Link to="/TechnicalChallenges" onClick={toggleMenu}>Technical Challenges</Link> {/* Añadido onClick */}
+            <li style={{ textDecoration: "none", color: "inherit" }}>
+              <Link to="/TechnicalChallenges" onClick={toggleMenu}>
+                Technical Challenges
+              </Link>{" "}
+              {/* Añadido onClick */}
             </li>
-            <li style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Link to="/studies" onClick={toggleMenu}>Studies</Link> {/* Añadido onClick */}
+            <li style={{ textDecoration: "none", color: "inherit" }}>
+              <Link to="/studies" onClick={toggleMenu}>
+                Studies
+              </Link>{" "}
+              {/* Añadido onClick */}
             </li>
-            <li style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Link to="/sponsors" onClick={toggleMenu}>Sponsors</Link> {/* Añadido onClick */}
+            <li style={{ textDecoration: "none", color: "inherit" }}>
+              <Link to="/sponsors" onClick={toggleMenu}>
+                Sponsors
+              </Link>{" "}
+              {/* Añadido onClick */}
             </li>
-            <li style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Link to="/AuditoriasOEntrevistas" onClick={toggleMenu}>Auditorias & Entrevistas</Link> {/* Añadido onClick */}
+            <li style={{ textDecoration: "none", color: "inherit" }}>
+              <Link to="/AuditoriasOEntrevistas" onClick={toggleMenu}>
+                Audits & Interviews
+              </Link>{" "}
+              {/* Añadido onClick */}
             </li>
-            <li style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Link to="/" onClick={toggleMenu}>Home</Link> {/* Añadido onClick */}
+            <li style={{ textDecoration: "none", color: "inherit" }}>
+              <Link to="/" onClick={toggleMenu}>
+                Home
+              </Link>{" "}
+              {/* Añadido onClick */}
             </li>
           </ul>
         </div>
