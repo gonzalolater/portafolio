fde9e47d78f160f638d81fe350b8cb32f94d93d3
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2025-04-21 21:16:16 -0300

    Agregar componente About para redirigir al repositorio de GitHub

---
+++ my-portfolio-client/src/components/About.js
@@ -0,0 +1,8 @@
+
+const About = () => {
+  // Redirige al repositorio de GitHub
+  window.location.href = "https://github.com/gonzalolater/portafolio";
+  return null; // No renderiza nada porque redirige automáticamente
+};
+
+export default About;
