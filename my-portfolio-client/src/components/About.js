ac94a7430780abeb2096f69e86a850f33a05c533
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2025-04-23 22:02:43 -0300

    Actualizar el título de la sección About y agregar enlaces a Devpost y GitHub

--- my-portfolio-client/src/components/About.js
+++ my-portfolio-client/src/components/About.js
@@ -3,7 +3,17 @@ import React from 'react';
 const About = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>About This Portfolio</h1>
+      <h1>About This Portfolio TOP 25 Global Microsoft Azure Hackaton</h1>
+      <img
+        src="https://github.com/gonzalolater/portafolio/assets/42863568/bfeb85a4-e013-4bb5-a8ab-444eb59ecfe2"
+        alt="Portfolio Preview"
+        style={{
+          display: 'block',
+          margin: '20px auto', // Centra la imagen horizontalmente
+          maxWidth: '80%', // Ajusta el tamaño de la imagen para que no sea demasiado grande
+          height: 'auto',
+        }}
+      />
       <p>
         This portfolio was created by Gonzalo Daniel Aguilar. You can find the source code on GitHub.
       </p>
@@ -23,6 +33,24 @@ const About = () => {
         }}
       >
         Go to GitHub Repository
+      </a>
+      <br />
+      <a
+        href="https://devpost.com/software/portfolio-fullstack-chat-with-ia"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: 'inline-block',
+          marginTop: '20px',
+          padding: '10px 20px',
+          backgroundColor: '#03a9f4', // Color azul para diferenciarlo del botón de GitHub
+          color: 'white',
+          textDecoration: 'none',
+          borderRadius: '5px',
+          fontSize: '16px',
+        }}
+      >
+        Go to Devpost
       </a>
     </div>
   );
