90a285a04da0edc06680a6e02f397e864cce5a16
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2025-05-02 21:39:53 +0000

    Refactor app.js to improve error handling, configure Mercado Pago, and streamline route management

--- my-portfolio-backend/src/app.js
+++ my-portfolio-backend/src/app.js
@@ -2,10 +2,19 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js";
-import taksRoutes from "./routes/tasks.routes.js";
+import helmet from "helmet";
 import { FRONTEND_URL } from "./config.js";
-import mercadopago from "mercadopago";
+import mercadopago from "./config/mercadopago.js";
+import fs from "fs";
+import path from "path";
+
+if (!FRONTEND_URL) {
+  throw new Error("FRONTEND_URL no está definido en las variables de entorno.");
+}
+
+if (!process.env.MERCADOPAGO_ACCESS_TOKEN) {
+  throw new Error("El token de acceso de Mercado Pago no está configurado.");
+}
 
 // Configura las credenciales de Mercado Pago
 mercadopago.configure({
@@ -32,6 +41,11 @@ mercadopago.preferences
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
 app.use(
   cors({
     credentials: true,
@@ -39,20 +53,32 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(cookieParser());
+app.use(helmet());
 
-app.use("/api/auth", authRoutes);
-app.use("/api", taksRoutes);
+const routesPath = path.join(__dirname, "routes");
+fs.readdirSync(routesPath).forEach((file) => {
+  const route = require(`./routes/${file}`);
+  app.use(`/api/${file.split(".")[0]}`, route);
+});
 
 if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    console.log(path.resolve("client", "dist", "index.html"));
     res.sendFile(path.resolve("client", "dist", "index.html"));
   });
+} else {
+  console.log("Modo desarrollo activado");
 }
+
+// Manejo de errores global
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
