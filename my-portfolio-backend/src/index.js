d12f47b63207b3ba0edf8e819c4a93ca48c6b843
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2025-05-08 08:10:21 +0000

    Add mercadopago configuration to index.js for access token setup

--- my-portfolio-backend/src/index.js
+++ my-portfolio-backend/src/index.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
+import mercadopago from "mercadopago";
+
+// Configura el token de acceso
+mercadopago.configurations.setAccessToken("YOUR_ACCESS_TOKEN");
 
 async function main() {
   try {
