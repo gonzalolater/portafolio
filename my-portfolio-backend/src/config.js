7172e2d5baaffe47fcc1ee05de1d6a5f708a839b
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2025-05-22 02:12:57 +0000

    Añadir configuración de acceso a MercadoPago en el archivo de configuración

--- my-portfolio-backend/src/config.js
+++ my-portfolio-backend/src/config.js
@@ -1,3 +1,6 @@
+import mercadopago from "mercadopago";
+mercadopago.accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN;
+
 export const PORT = process.env.PORT || 3000;
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb+srv://<user>:<password>@portfolio.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000";
