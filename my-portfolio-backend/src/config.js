499ab87579f68b786967b62fdd47c274bb482a0e
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2025-05-24 21:15:41 +0000

    Update MongoDB URI in config.js for correct connection string

--- my-portfolio-backend/src/config.js
+++ my-portfolio-backend/src/config.js
@@ -3,7 +3,7 @@ mercadopago.accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN;
 
 export const PORT = process.env.PORT || 3000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://<user>:<password>@portfolio.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000";
+  process.env.MONGODB_URI || "mongodb+srv://<portfoliodb>:<NQk5yl1wsPPC067U>@cluster-portfoliodb.po8zqvo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-portfoliodb";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
