5c2d9d28ab1c78bf4a30fed3ef46e5baa00377bc
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2025-06-06 21:01:13 +0000

    Add responsive image gallery to About component

--- my-portfolio-client/src/components/About.js
+++ my-portfolio-client/src/components/About.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './About.css'; // Asegúrate de que la ruta sea correcta
 
 const About = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1
         style={{
-          margin: '5px', // Margen de 5px alrededor del borde
-          border: '5px solid black', // Borde de 5px de color negro
-          padding: '10px', // Espaciado interno para que el texto no toque el borde
+          margin: '5px',
+          border: '5px solid black',
+          padding: '10px',
         }}
       >
         About This Portfolio TOP 25 Global Microsoft Azure Hackaton
@@ -17,14 +18,14 @@ const About = () => {
         alt="Portfolio Preview"
         style={{
           display: 'block',
-          margin: '20px auto', // Centra la imagen horizontalmente
-          maxWidth: '80%', // Ajusta el tamaño de la imagen para que no sea demasiado grande
+          margin: '20px auto',
+          maxWidth: '80%',
           height: 'auto',
         }}
       />
       <p
         style={{
-          margin: '10px', // Agrega un margen de 10px alrededor del párrafo
+          margin: '10px',
         }}
       >
         This portfolio was created by Gonzalo Daniel Aguilar. You can find the source code on GitHub.
@@ -55,7 +56,7 @@ const About = () => {
           display: 'inline-block',
           marginTop: '20px',
           padding: '10px 20px',
-          backgroundColor: '#03a9f4', // Color azul para diferenciarlo del botón de GitHub
+          backgroundColor: '#03a9f4',
           color: 'white',
           textDecoration: 'none',
           borderRadius: '5px',
@@ -64,6 +65,23 @@ const About = () => {
       >
         Go to Devpost
       </a>
+      {/* Imágenes lado a lado, responsivas */}
+      <div className="about-gallery-container">
+        <a href="https://ibb.co/RT4pVDHD" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://i.ibb.co/k26gWScS/Imagen-de-Whats-App-2025-06-05-a-las-01-59-08-bc5ea2b1.jpg"
+            alt="Imagen-de-Whats-App-2025-06-05-a-las-01-59-08-bc5ea2b1"
+            className="about-gallery-img"
+          />
+        </a>
+        <a href="https://ibb.co/LDyN9vM5" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://i.ibb.co/wZqcpYTs/Imagen-de-Whats-App-2025-06-05-a-las-01-59-30-0b3f4994.jpg"
+            alt="Imagen-de-Whats-App-2025-06-05-a-las-01-59-30-0b3f4994"
+            className="about-gallery-img"
+          />
+        </a>
+      </div>
     </div>
   );
 };
