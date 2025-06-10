4ab433fbca13ddcb25dfea4195dc407824434d4a
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2025-06-10 01:27:34 +0000

    Refactor MongoDB URI configuration for improved security and clarity

--- my-portfolio-backend/src/config.js
+++ my-portfolio-backend/src/config.js
@@ -1,10 +1,8 @@
 import mercadopago from "mercadopago";
+
 mercadopago.accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN;
 
 export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://portfolio-db:db-asd123asd123@portfolio.jicyd2d.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
+export const MONGODB_URI =  process.env.MONGODB_URI; 
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
-
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
